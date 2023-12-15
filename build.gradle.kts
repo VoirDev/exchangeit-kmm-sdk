e261c25c2c495ad4335a856079245fa52053d431
Garik Bezruchko <garik.bezruchko@yandex.ru>
2023-12-15 19:04:10 +0300

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,10 +20,8 @@ allprojects {
 }
 
 nexusPublishing {
-    // Configure maven central repository
-    // https://github.com/gradle-nexus/publish-plugin#publishing-to-maven-central-via-sonatype-ossrh
     repositories {
-        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
+        sonatype {
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
         }
@@ -117,11 +115,6 @@ publishing {
     }
 
     repositories {
-        maven {
-            name = "Maven"
-            setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-        }
-
         maven {
             name = "Github"
             setUrl("https://maven.pkg.github.com/VoirDev/exchangeit-kmm-sdk")
